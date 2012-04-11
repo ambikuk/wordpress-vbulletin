3c7ff207e9ec52c88f11bea9dacdccc6177116de
Ivan <ivanpetrus@gmail.com>
2012-04-11 13:26:58 +0700

    Copyright message

--- wpvb.php
+++ wpvb.php
@@ -6,10 +6,10 @@
 /*
 Plugin Name: VbWP 
 Plugin URI: http://wordpress.org/extend/plugins/hello-dolly/
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
-Author: ambikuk salawean
-Version: 1.6
-Author URI: http://ma.tt/
+Description: Wordpress - vBulletin Single Sign On
+Author: Ndang, Ivan
+Version: 0.1
+Author URI: http://technolyze.net/
 */
 
 define('VB_COOKIE_PREFIX', 'bb');
