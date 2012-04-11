c36175bc07aa536a883d236befde548b18a156da
Ivan <ivanpetrus@gmail.com>
2012-04-11 13:23:44 +0700

    $vbdb and user sync

--- wpvb.lib.inc.php
+++ wpvb.lib.inc.php
@@ -97,20 +97,6 @@ function wpvb_db_query($query) {
   * @see db_query_range
  */
 function wpvb_db_query_range($query) {
-  $args = func_get_args();
-  $count = array_pop($args);
-  $from = array_pop($args);
-  array_shift($args);
-
-  $query = db_prefix_tables($query);
-  if (isset($args[0]) and is_array($args[0])) { // 'All arguments in one array' syntax
-    $args = $args[0];
-  }
-  _db_query_callback($args, TRUE);
-  $query = preg_replace_callback(DB_QUERY_REGEXP, '_db_query_callback', $query);
-  $query .= ' LIMIT '. (int)$count .' OFFSET '. (int)$from;
-  $result = _db_query($query);
-  return $result;
 }
 
 /**
@@ -130,31 +116,39 @@ function wpvb_db_last_insert_id($table, $field) {
  * @see wpvb.admin-pages.inc
  */
 function _wpvb_init_user_map() {
+	global $wpdb;
+	$vpdb = wpvb_db();
   $users = $vbusers = array();
-  // Fetch all users in Drupal.
-  $result = db_query("SELECT uid, name FROM {users}");
-  while ($user = db_fetch_array($result)) {
-    $users[$user['name']] = $user['uid'];
+  // Fetch all users in Wordpress.
+  $result = $wpdb->get_results("SELECT ID, user_login FROM $wpdb->users");
+  foreach($result as $row) {
+    $users[$row->user_login] = $row->ID;
   }
   // Fetch all users in vBulletin.
-  $result = wpvb_db_query("SELECT userid, username FROM {user}");
+  $result = $vbdb->get_results("SELECT userid, username FROM $vbdb->user");
   // Insert all vB users who already exist in Drupal with corresponding username
   // into our mapping table.
-  while ($vbuser = db_fetch_array($result)) {
-    if (isset($users[$vbuser['username']])) {
-      db_query("INSERT INTO {wpvb_users} (uid, userid) VALUES (%d, %d)", $users[$vbuser['username']], $vbuser['userid']);
-    }
-  }
+	foreach($result as $vbrow) {
+		if(isset($users[$vbrow->username])) {
+			$wpdb->insert('vb_users', array(
+				'wp_id' => $users[$vbrow->username],
+				'vb_id' => $vbrow->userid
+			), array(
+				'%d',
+				'%d'
+			));
+		}
+	}
 }
 
 /**
  * @return wpdb 
  */
 function wpvb_db() {
-	static $wpdb;
-	if(!isset($wpdb)) {
-		$wpdb = new wpdb(WPVB_DB_USER, WPVB_DB_PASSWORD, WPVB_DB_NAME, WPVB_DB_HOST);
-		$wpdb->set_prefix(get_option('wpvb_db_prefix', ''));
+	static $vbdb;
+	if(!isset($vbdb)) {
+		$vbdb = new wpdb(WPVB_DB_USER, WPVB_DB_PASSWORD, WPVB_DB_NAME, WPVB_DB_HOST);
+		$vbdb->set_prefix(get_option('wpvb_db_prefix', ''));
 	}
-	return $wpdb;
+	return $vbdb;
 }
